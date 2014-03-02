21d3fea0a93e745260767d8dd9c0375682536249
Dane Stier <DaneStier@u.northwestern.edu>
2014-03-02 03:19:51 -0600

    Deleting Unnecessary/Changing Names
    Don’t worry about it.

--- Divs/Maindivs.js
+++
@@ -1,5 +0,0 @@
-
-function handleResize() {
-	var mapElement = document.getElementById("logo");
-	mapElement.style.height = String((mapElement.offsetWidth * 0.5)) + "px";
-}
--- menu.js
+++
@@ -1,8 +0,0 @@
-
-
-$(document).ready(function() {
-	$("#navigation ul a li").click(function(event) {
-		$("#navigation li").removeClass("selected");
-		$(this).addClass("selected");
-	});
-});
